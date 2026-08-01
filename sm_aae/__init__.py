Sharathvc23/sm-bridge

fix: derive __version__ from distribution metadata, not a literal
__version__ was a hardcoded literal — a second copy of pyproject's version
with nothing comparing them. The value was correct at the time of this
change, which is exactly the point: correct-today is the state every
package in this org was in immediately before it drifted.

Two of them already did. sm-provision 0.1.0 shipped __version__ == "0.0.1"
against dist metadata that said 0.1.0, and sm-authority 0.1.0 shipped the
same wrong value — one shared template whose first release forgot to set
the literal, inherited by everything built from it. sm-arp had no
__version__ at all, which is worse: getattr(mod, "__version__", default)
silently returns the caller's fallback, so a consumer's check reports its
own guess and looks like it succeeded.

The cost is not cosmetic. A consumer doing a defensive runtime check to
avoid a vulnerable release reads the wrong number and cannot detect it —
that happened: sm-authority 0.1.0 carried the OIDC confused-deputy and
reported "0.0.1", leaving the dependency floor as the only enforcement
point.

Patching the value is not the fix. sm-bridge 0.6.0's changelog says it
fixed __version__; it corrected the VALUE, the wheel still carried a
literal, and it was set to drift again at the next bump. So this changes
the mechanism: __version__ derives from importlib.metadata, pyproject
stays the single declared source, and the installed wheel cannot
misreport itself. An uninstalled source tree reports 0.0.0.dev0 — visibly
a placeholder, not a plausible wrong number.

Four tests, ported verbatim from the reviewed convention in
sm-authority#4 (3cf125b) rather than redesigned per repo. The load-bearing
one asserts the ABSENCE of a literal in the module source, not equality:
equality passes the moment someone re-adds a literal that happens to match
today, and only fails after the next bump has already drifted. Proven
failing-before in that exact shape — with the literal restored to the

sm_bridge/__init__.py
@@ -26,6 +26,9 @@ Usage:
 See https://github.com/projnanda for the official NANDA specification.
 """
 
+from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
+from importlib.metadata import version as _dist_version
+
 from .binding_write import (
     BindingStore,
     BindingWriteRequest,
@@ -82,7 +85,15 @@ from .trust import ProofResult, ProofStatus, TrustProfile, TrustRegistry
 # import them from their submodules (e.g. from sm_bridge.trust.ans_scitt import AnsScittProfile)
 # so a core-only install never imports cryptography/cbor2/dnspython.
 
-__version__ = "0.6.0"
+# Derived from installed distribution metadata, never hand-maintained. A literal
+# here is a second copy of pyproject's ``version`` with nothing comparing them —
+# the shape that shipped sm-provision 0.1.0 reporting "0.0.1" and sm-authority
+# 0.1.0 reporting the same wrong value from the same template. Correct today is
+# not the test; it drifts at the next bump.
+try:  # pragma: no cover - trivial branch, both sides asserted in tests
+    __version__ = _dist_version("sm-bridge")
+except _PackageNotFoundError:  # running from a source tree, not installed
+    __version__ = "0.0.0.dev0"
 __all__ = [
     # Core Models
     "SmAgentFacts",

tests/test_version_metadata.py
@@ -1,0 +1,61 @@
+"""``__version__`` must equal the installed distribution version.
+
+A literal is a second copy of pyproject's ``version`` with nothing comparing
+them. That is not a hypothetical shape in this org: sm-provision 0.1.0 shipped
+``__version__ == "0.0.1"`` against correct dist metadata, and sm-authority 0.1.0
+shipped the same wrong value from the same template. sm-bridge 0.6.0 then
+"fixed" its version by correcting the *value* — its wheel still carried a
+literal, so it was set to drift again at the next bump.
+
+sm-bridge was correct at the time of this change. That is the point: correct-today
+is exactly the state these were all in right before they drifted, so the guard
+below asserts on the *duplication* rather than on the value.
+"""
+
+from __future__ import annotations
+
+import re
+from importlib.metadata import version as dist_version
+from pathlib import Path
+
+import pytest
+import tomllib
+
+import sm_bridge
+
+
+def test_dunder_version_equals_installed_distribution_version() -> None:
+    assert sm_bridge.__version__ == dist_version("sm-bridge")
+
+
+def test_dunder_version_is_not_the_uninstalled_sentinel() -> None:
+    # The source-tree fallback must never reach an installed environment; if it
+    # does, every consumer's version check silently reads a placeholder.
+    assert sm_bridge.__version__ != "0.0.0.dev0"
+
+
+def test_module_source_declares_no_version_literal() -> None:
+    """The regression guard proper.
+
+    Equality above passes the moment someone re-adds a literal that happens to
+    match today, and only fails after the next bump has already drifted. This
+    fails on the duplication itself, so the shape is rejected while it is still
+    correct.
+    """
+    src = Path(sm_bridge.__file__).read_text(encoding="utf-8")
+    literals = re.findall(r'^__version__\s*=\s*["\']', src, flags=re.MULTILINE)
+    assert not literals, (
+        "__version__ must be derived from distribution metadata, not assigned a "
+        "literal — a literal is a second copy of pyproject's version and drifts."
+    )
+
+
+def test_pyproject_remains_the_single_declared_source() -> None:
+    pyproject = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    if not pyproject.exists():  # installed-only test run
+        pytest.skip("source checkout not present")
+    declared = tomllib.loads(pyproject.read_text(encoding="utf-8"))["project"]["version"]
+    assert declared == dist_version("sm-bridge"), (
+        "pyproject declares the version; a mismatch here means the installed "
+        "wheel was built from a different tree than this checkout."
+    )

Sharathvc23/sm-aae

sm_aae/__init__.py
@@ -21,6 +21,9 @@ Public API::
 
 from __future__ import annotations
 
+from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
+from importlib.metadata import version as _dist_version
+
 from sm_aae.envelope import (
     OUTCOMES,
     Envelope,
@@ -31,7 +34,15 @@ from sm_aae.envelope import (
     verify_envelope,
 )
 
-__version__ = "0.1.0"
+# Derived from installed distribution metadata, never hand-maintained. A literal
+# here is a second copy of pyproject's ``version`` with nothing comparing them —
+# the shape that shipped sm-provision 0.1.0 reporting "0.0.1" and sm-authority
+# 0.1.0 reporting the same wrong value from the same template. Correct today is
+# not the test; it drifts at the next bump.
+try:  # pragma: no cover - trivial branch, both sides asserted in tests
+    __version__ = _dist_version("sm-aae")
+except _PackageNotFoundError:  # running from a source tree, not installed
+    __version__ = "0.0.0.dev0"
 
 __all__ = [
     "OUTCOMES",

tests/test_version_metadata.py
@@ -1,0 +1,61 @@
+"""``__version__`` must equal the installed distribution version.
+
+A literal is a second copy of pyproject's ``version`` with nothing comparing
+them. That is not a hypothetical shape in this org: sm-provision 0.1.0 shipped
+``__version__ == "0.0.1"`` against correct dist metadata, and sm-authority 0.1.0
+shipped the same wrong value from the same template. sm-bridge 0.6.0 then
+"fixed" its version by correcting the *value* — its wheel still carried a
+literal, so it was set to drift again at the next bump.
+
+sm-aae was correct at the time of this change. That is the point: correct-today
+is exactly the state these were all in right before they drifted, so the guard
+below asserts on the *duplication* rather than on the value.
+"""
+
+from __future__ import annotations
+
+import re
+import tomllib
+from importlib.metadata import version as dist_version
+from pathlib import Path
+
+import pytest
+
+import sm_aae
+
+
+def test_dunder_version_equals_installed_distribution_version() -> None:
+    assert sm_aae.__version__ == dist_version("sm-aae")
+
+
+def test_dunder_version_is_not_the_uninstalled_sentinel() -> None:
+    # The source-tree fallback must never reach an installed environment; if it
+    # does, every consumer's version check silently reads a placeholder.
+    assert sm_aae.__version__ != "0.0.0.dev0"
+
+
+def test_module_source_declares_no_version_literal() -> None:
+    """The regression guard proper.
+
+    Equality above passes the moment someone re-adds a literal that happens to
+    match today, and only fails after the next bump has already drifted. This
+    fails on the duplication itself, so the shape is rejected while it is still
+    correct.
+    """
+    src = Path(sm_aae.__file__).read_text(encoding="utf-8")
+    literals = re.findall(r'^__version__\s*=\s*["\']', src, flags=re.MULTILINE)
+    assert not literals, (
+        "__version__ must be derived from distribution metadata, not assigned a "
+        "literal — a literal is a second copy of pyproject's version and drifts."
+    )
+
+
+def test_pyproject_remains_the_single_declared_source() -> None:
+    pyproject = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    if not pyproject.exists():  # installed-only test run
+        pytest.skip("source checkout not present")
+    declared = tomllib.loads(pyproject.read_text(encoding="utf-8"))["project"]["version"]
+    assert declared == dist_version("sm-aae"), (
+        "pyproject declares the version; a mismatch here means the installed "
+        "wheel was built from a different tree than this checkout."
+    )